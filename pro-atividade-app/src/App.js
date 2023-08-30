d9d702d969527108e62b9253c999c46f89e1866b
Lucas Aboud <lucas_aboud@hotmail.com>
2023-08-30 17:44:51 -0300

    estrutura geral conflito id

--- pro-atividade-app/src/App.js
+++ pro-atividade-app/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 let initialState = [
   {
     id: 1,
-    prioridade: 'Baixa',
+    prioridade: '1',
     titulo: 'Primeira atividade',
     descrição: 'Primeira atividade',
   },
@@ -29,6 +29,32 @@ function App() {
     setAtividades([...atividades, {...atividade}]);
   }
 
+  function prioridadeLabel(param) {
+    switch(param) {
+      case '1':
+        return 'Baixa'
+      case '2':
+        return 'Normal'
+      case '3':
+        return 'Alta'
+      default:
+        return 'Não Definido'
+    }
+  }
+
+  function rostoPrioridade(param) {
+    switch(param) {
+      case '1':
+        return 'smile'
+      case '2':
+        return 'meh'
+      case '3':
+        return 'frown'
+      default:
+        return 'Não Definido'
+    }
+  }
+
   return (
     <>
       <div className="div justify-content-center ms-5 me-5 pt-5">
@@ -38,21 +64,10 @@ function App() {
               ID:
             </label>
             <input 
-              id="id" 
+              id="id"
               type="text" 
-              className="form-control" 
-              placeholder='Nº Id...' 
-            />
-          </div>
-          <div className="col-md-6">
-            <label className="form-label">
-              Descrição: 
-            </label>
-            <input 
-              id="descrição" 
-              type="text" 
-              className="form-control" 
-              placeholder='Descrição'
+              className="form-control"
+              placeholder='Nº Id...'
             />
           </div>
           <div className="col-md-6">
@@ -67,6 +82,17 @@ function App() {
             />
           </div>
           <div className="col-md-6">
+            <label className="form-label">
+              Descrição: 
+            </label>
+            <input 
+              id="descrição" 
+              type="text" 
+              className="form-control" 
+              placeholder='Descrição'
+            />
+          </div>
+          <div className="col-md-6">
               <label className="form-label">Prioridade:</label>
               <select id="prioridade" className="form-select">
                 <option defaultValue="0">Selecione...</option>
@@ -75,22 +101,21 @@ function App() {
                 <option value="3">Alta</option>
               </select>
           </div>
-          
           <hr />
           <div className='col-12'>
             <button 
-              className='btn-outline-secondary' 
+              className='btn btn-outline-primary' 
               onClick={addAtividade}
             > 
-              + Atividade
+              Enviar
             </button>  
           </div>
         </form>
 
         <div className="mt-3">
-            {atividades.map(ativ => (
-              <div key={ativ.id} className="card mb-2 shadow-sm">
-                <div class="card-body">
+            {atividades.map((ativ,index) => (
+              <div key={index} className="card mb-2 shadow-sm">
+                <div className="card-body">
                   <div className= "d-flex justify-content-between">
                     <h5 className='card-tittle'>
                     <span className="badge bg-secondary me-1">{ativ.id}</span>
@@ -99,12 +124,12 @@ function App() {
                     <h6>
                       Prioridade: 
                       <span className='ms-1 text-black'>
-                        <i className='me-1 far fa-smile'></i>
-                        {ativ.prioridade}
+                        <i className={'me-1 far fa-' + rostoPrioridade(ativ.prioridade) }></i>
+                        {prioridadeLabel(ativ.prioridade)}
                       </span>
                     </h6>
                   </div>
-                  <p class="card-text">{ativ.descrição}</p>
+                  <p className="card-text">{ativ.descrição}</p>
                   <div className="d-flex justify-content-end pt-2 m-0 border-top">
                     <button className="btn btn-sm btn-outline-primary me-2">
                       <i className='fas fa-pen me-2'></i>
